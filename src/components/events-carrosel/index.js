4f723da2f9e1cb1431fe3fb06786d883698be803
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-07-07 09:30:40 -0300

    feat(14/eventos-carrosel): passando o codigo para o meu pareamento - @angeloneves19 - @willianfreitas01 - @oGuilhermeSoares

--- src/components/events-carrosel/index.js
+++ src/components/events-carrosel/index.js
@@ -1,111 +1,115 @@
-import foto from '../parceires/fot.png'
-import EventsComponent from './styled.js'
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import { Navigation } from 'swiper'
-const linkParceire = process.env.REACT_APP_LINK_PARCEIRO
+import foto from "../parceires/fot.png"
+import EventsComponent from "./styled.js"
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
+import { Navigation } from "swiper"
 
 const Events = () => {
   return (
-    <EventsComponent style={{
-      background: '#F5BC4A'
-    }}>
-      <h2>Parceires</h2>
-      <p>
-        A Nossa Casa é uma Associação sem fins lucrativos, composta por profissionais, artistas e ativistas do Município de Guarulhos.
-      </p>
+    <EventsComponent
+      style={{
+        background: "#FFFFFF",
+      }}
+    >
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
         navigation={true}
         breakpoints={{
-          '@0.00': {
+          "@0.00": {
             slidesPerView: 1,
-            spaceBetween: 10
+            spaceBetween: 10,
           },
-          '@0.75': {
+          "@0.75": {
             slidesPerView: 2,
-            spaceBetween: 20
+            spaceBetween: 20,
           },
-          '@1.00': {
+          "@1.00": {
             slidesPerView: 3,
-            spaceBetween: 40
-          }
+            spaceBetween: 40,
+          },
         }}
         modules={[Navigation]}
         className="mySwiper"
       >
-        <section className='events'>
+        <section className="events">
           <div className="swiper-slide">
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-            <div className='evento'>
-            <img src={foto} />
-            <p>22 Oct 2018</p>
-            <h3>Impact Of Extrinsic Motivation On Intrinsic Motivation</h3>
-            <button>VER MAIS</button>
-          </div>
+              <div className="evento">
+                <img src={foto} />
+                <p className="date">22 Oct 2018</p>
+                <h3 className="title">
+                  Impact Of Extrinsic Motivation On Intrinsic Motivation
+                </h3>
+                <button className="button">VER MAIS</button>
+              </div>
             </SwiperSlide>
           </div>
         </section>
       </Swiper>
-      <div className='styled-button'>
-        <div className='button-parceire'>
-          <a target='_blank' href={linkParceire} rel="noreferrer">
-            Ser parceire
-          </a>
-        </div>
-      </div>
-    </EventsComponent >
+    </EventsComponent>
   )
 }
 export default Events
