ae1cdb1c32571146df678bcc72e6d9d68139bdfd
cintia89 <cintia.silvadeooliveira@gmail.com>
2023-07-11 10:30:04 -0300

    refactor:(components/galery-modal)implementar o swipper - @cintia89 @angeloneves19

--- src/components/galery-modal/index.js
+++ src/components/galery-modal/index.js
@@ -2,73 +2,71 @@ import { ModalContainer } from './styled'
 import { useState } from 'react'
 import closeIcon from './close-icon.svg'
 import { buttonTextByTypes } from '../../constants/constants'
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import { FreeMode, Navigation, Thumbs } from 'swiper'
-// import nossacasa from './nossacasa.png'
-// import casa from './casa.png'
-// import casadentro from './casadentro.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Navigation, Thumbs } from 'swiper'
+import casadentro from './casadentro.png'
 
 import 'swiper/css'
 import 'swiper/css/free-mode'
 import 'swiper/css/navigation'
 import 'swiper/css/thumbs'
 
-// const IMAGES = [nossacasa, casa, casadentro, nossacasa, nossacasa, nossacasa]
+const IMAGES = [casadentro, casadentro, casadentro, casadentro, casadentro]
 
 const ModalGallery = ({ type }) => {
   const [toggleGallery, setToggleGallery] = useState(false)
-  // const [thumbsSwiper, setThumbsSwiper] = useState(null)
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
 
   const toggleGalleryHandler = () => {
     setToggleGallery(!toggleGallery)
   }
-  // const closeAndRefresh = () => {
-  //   toggleGalleryHandler()
-  //   window.location.reload()
-  // }
+  const closeAndRefresh = () => {
+    toggleGalleryHandler()
+    window.location.reload()
+  }
 
   return <>
        <button onClick={toggleGalleryHandler}>{buttonTextByTypes[type]}</button>
 
     {toggleGallery &&
-      <ModalContainer theme={type}>
-        <img className='exit' src={closeIcon} onClick={toggleGalleryHandler}/>
+      <ModalContainer theme = {type}>
+      <img className='exit' src={closeIcon} onClick={closeAndRefresh} />
         {type === 'about' &&
           <>
-            <h2 className='about-title'>Sobre Nossa Casa</h2>
-            <div className='content-container'>
-              <div className='topic'>
+            <h2 className = 'about-title'>Sobre Nossa Casa</h2>
+            <div className = 'content-container'>
+            <div className = 'topic'>
                 <h3>Nossa Historia Ate Aqui</h3>
                 <p>
                   Em 2018 a Nossa Casa nasce com o intuito de oferecer à população acesso à serviços, produtos e atividades nas áreas de saúde mental, terapias integrativas, cultura e arte, entendendo tais ações como promoção e garantia de direitos à todes, com foco na acessibilidade à pessoas em situação de vulnerabilidade.
                 </p>
               </div>
-              <div className='topic'>
+              <div className = 'topic'>
               <h3>Como somos conhecidos</h3>
                 <p>
                   A Nossa Casa passa então à ser reconhecida como um centro cultural, de formação, fomentação e garantia de direitos à todes, sobretudo à população negra, mulheres e LGBTQIAP+ articulando e promovendo eventos, lançamentos musicais, saraus, exposições áudio visuais, palestras, oficinas, etc.
                 </p>
               </div>
-              <div className='topic'>
+              <div className = 'topic'>
               <h3>Nosso inicio</h3>
                 <p>
                   No primeiro ano de funcionamento a Nossa Casa atendeu centenas de pessoas nos serviços de psicoterapia, massoterapia, yoga, nutrição, psicopedagogia, arte terapia, rodas de conversa, formações, entre outros, á valores acessíveis ou ainda em parceria com entidades do terceiro setor do Município atendendo seus usuários.
                 </p>
               </div>
-              <div className='topic'>
+              <div className = 'topic'>
               <h3>Curiosidades</h3>
                 <p>
                   No ano de 2022 a Nossa Casa foi contratada pela Secretaria de Igualdade Racial para organização da Festa Preta
                   oficial da cidade, em comemoração ao dia da Consciência Negra.
                 </p>
               </div>
-              <div className='topic'>
+              <div className = 'topic'>
               <h3>Profissionais</h3>
                 <p>
                   Os profissionais que compunham a Nossa Casa, atuavam também de maneira autônoma gerando renda para si e contribuindo para a manutenção do coletivo.
                 </p>
               </div>
-              <div className='topic'>
+              <div className = 'topic'>
               <h3>Como funciona</h3>
                 <p>
                   Funcionando de maneira independente e colaborativa o coletivo embora tenha estabelecido parcerias com algumas entidades e outros atores de coletivos semelhantes na cidade, não estabeleceu nenhuma forma de conveniamento ou parceria contínua com qualquer órgão publico nesse período.
@@ -78,7 +76,42 @@ const ModalGallery = ({ type }) => {
           </>
         }
         {
-          <h2 className='h2-modal-galery'>Fotos da Nossa Casa</h2>
+          type === 'gallery' &&
+        <>
+        <div className='topo'>
+            <h2 className='h2-modal-galery'>Fotos da Nossa Casa</h2>
+        </div>
+          <Swiper
+            style={{
+              '--swiper-navigation-color': '#FFF',
+              '--swiper-pagination-color': '#FFF'
+            }}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className='swiper-slide-view'
+          >
+              {IMAGES.map((image, index) => (
+                <SwiperSlide key={index}>
+                  <img src={image} />
+                </SwiperSlide>
+              ))}
+            </Swiper><Swiper
+              onSwiper={setThumbsSwiper}
+              spaceBetween={10}
+              slidesPerView={6}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className='swiper-thumbnails'
+            >
+              {IMAGES.map((image, index) => (
+                <SwiperSlide key={index}>
+                  <img src={image} />
+                </SwiperSlide>
+              ))}
+            </Swiper></>
         }
       </ModalContainer>}
   </>
--- src/components/galery-modal/styled.js
+++ src/components/galery-modal/styled.js
@@ -160,4 +160,112 @@ p {
 
 
 }
-  `
+//modal galerias
+.topo {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 25px;
+    padding-right: 20px;
+  }
+
+  .topo > .h2-modal-galery {
+    margin: 0;
+    margin-left: 25%;
+    font-size: 2.25rem;
+  }
+
+  .exit {
+    display: inline;
+  }
+.swiper {
+    width: 100%;
+    height: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #A49D9D;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .swiper-slide-view {
+    height: 65%;
+    width: 50%;
+  }
+
+  .swiper-thumbnails {
+    height: 13%;
+    width: 50%;
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+
+  .swiper-thumbnails .swiper-slide {
+    width: 25%;
+    height: 100%;
+    opacity: 0.4;
+  }
+
+  .swiper-thumbnails .swiper-slide-thumb-active {
+    opacity: 1;
+    border: 4px solid #F5BC4A;
+  }
+
+  @media (max-width: 1280px) {
+
+    .topo {
+      margin-bottom: 30px;
+    }
+
+    .topo > .h2-modal-galery {
+      margin-left: 20%;
+    }
+    .swiper-slide-view {
+      height: 65%;
+      width: 60%;
+    }
+
+    .swiper-thumbnails {
+      height: 12%;
+      width: 60%;
+      box-sizing: border-box;
+      padding: 10px 0;
+    }
+  }
+
+  @media (min-width: 1281px) {
+
+    .topo > .h2-modal-galery {
+      margin-left: 20%;
+    }
+    .swiper {
+      margin-top: 25px;
+    }
+    .swiper-slide-view {
+      height: 65%;
+      width: 60%;
+    }
+
+    .swiper-thumbnails {
+      height: 12%;
+      width: 60%;
+      box-sizing: border-box;
+      padding: 10px 0;
+    }
+  }
+`
