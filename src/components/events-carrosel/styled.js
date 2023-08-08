57024a5cf2e232ef21c413bab59bde3cbcfd4320
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-08-08 11:43:34 -0300

    feat(n/a): codigo da modal atualizado e funcionando -@angeloneves19 -@gabrielapsgomes -@JCdantss -@PabloHunter

--- src/components/events-carrosel/styled.js
+++ src/components/events-carrosel/styled.js
@@ -56,36 +56,6 @@ body {
   cursor: pointer;
 }
 
-.swiper-button-next {
-    position: absolute;
-    top: 50%;
-    width: calc(var(--swiper-navigation-size)/4*47);
-    height: var(--swiper-navigation-size);
-    margin-top: calc(0px - var(--swiper-navigation-size)/3);
-    
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: -15rem;
-    color: var(--swiper-navigation-color,var(--swiper-theme-color));
-}
-
-.swiper-button-prev{
-    position: absolute;
-    top: 50%;
-    width: calc(var(--swiper-navigation-size)/89*47);
-    height: var(--swiper-navigation-size);
-    margin-top: calc(0px - var(--swiper-navigation-size)/3);
-    
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: -15rem;
-    color: var(--swiper-navigation-color,var(--swiper-theme-color));
-}
-
 `
 
 export default EventsComponent
