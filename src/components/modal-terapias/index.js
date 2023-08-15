e296e3d9a0d34ec2d4751d66ecd6e9ec0ed2010b
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-08-15 11:27:42 -0300

    Refactor(components/modal-terapias/index.js): Ajustes sugeridos no pullresqueste resolvidos -@gabrielapsgomes -@angeloneves19

--- src/components/modal-terapias/index.js
+++ src/components/modal-terapias/index.js
@@ -5,9 +5,9 @@ import closeIcon from '../galery-modal/close-icon.svg'
 import { buttonModal } from '../../constants/constants'
 
 const ModalTherapies = () => {
-  const [modal, setModal] = useState(false)
+  const [showModal, setShowModal] = useState(false)
   const handleClick = () => {
-    setModal((modal) => !modal)
+    setShowModal(!showModal)
   }
   return (
     <>
@@ -16,7 +16,7 @@ const ModalTherapies = () => {
           {buttonModal}
         </button>
         <Modal
-          isOpen={modal}
+          isOpen={showModal}
           onRequestClose={handleClick}
           className="modal"
           overlayClassName="modal-overlay"
