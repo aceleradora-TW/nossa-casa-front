72cf6dea543739b0cc9342d7d52e27aee2377b76
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-08-16 10:43:11 -0300

    Implementando informacoes sobre os detalhes das oficinas na modal @gabrielapsgomes @angeloNeves19

--- src/components/modal-oficinas/index.js
+++ src/components/modal-oficinas/index.js
@@ -1,9 +1,22 @@
 import Modal from 'react-modal'
-import { useState } from 'react'
-import { CssModalGlobal } from '../global-styles'
+import { useState, useEffect } from 'react'
+import CssModalGlobal from '../css-modal/styled'
 import closeIcon from '../galery-modal/close-icon.svg'
+import { cms } from '../../service/client'
+import CssModal from './styled'
 
 const ModalWorkshops = () => {
+  const [attributes, setAttributes] = useState([])
+
+  useEffect(() => {
+    cms.get('api/workshops/?populate=foto_divulgacao').then((response) => {
+      const { data } = response.data
+      const workshops = data.map((data) => {
+        return data.attributes
+      })
+      setAttributes(workshops)
+    })
+  }, [])
   const [showModal, setShowModal] = useState(false)
   const handleClick = () => {
     setShowModal(!showModal)
@@ -14,6 +27,7 @@ const ModalWorkshops = () => {
         <button className="button-styled" onClick={handleClick}>
           Ver Mais
         </button>
+        <CssModal>
         <Modal
           isOpen={showModal}
           onRequestClose={handleClick}
@@ -21,7 +35,29 @@ const ModalWorkshops = () => {
           overlayClassName="modal-overlay"
         >
           <img className="button-modal" src={closeIcon} onClick={handleClick} />
+        <section>
+          <div className="detalhes">
+            <ul>
+              {attributes.map((workshops, key) => (
+                <li key={key}>
+                      <div>
+                      <h1 className="title">{workshops.nome}</h1>
+                      <h1 className="horario">{workshops.carga_horaria}</h1>
+                      <h1 className="local">{workshops.local}</h1>
+                      <h1 className="data-inicio">{workshops.data_inicio}</h1>
+                      <h1 className="data-fim">{workshops.data_fim}</h1>
+                      <h1 className="horario-inicio">{workshops.horario_inicio}</h1>
+                      <h1 className="horario-fim">{workshops.horario_fim}</h1>
+                      <h1 className="preco">{workshops.preco}</h1>
+                      <p className="descricao">{workshops.descricao}</p>
+                      </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
         </Modal>
+        </CssModal>
       </CssModalGlobal>
     </>
   )
---
+++ src/components/modal-oficinas/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const CssModal = styled.div`
+
+.title{
+ background-color: pink;
+ font-size: 10%;
+}
+`
+export default CssModal
