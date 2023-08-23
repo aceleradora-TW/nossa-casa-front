5efc5d689db2756b44aabbbc2f6731dbf5efbac9
Pablo Hunter <aceleradora@MacBook-Pro-do-Pablo.local>
2023-08-23 10:40:15 -0300

    fazendo alteracoes do slinet

--- src/components/events-carrosel/index.js
+++ src/components/events-carrosel/index.js
@@ -34,6 +34,7 @@ const Events = () => {
         <h1>EVENTOS</h1>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
+          Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been thes standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book. It has survived not only five centuries,
