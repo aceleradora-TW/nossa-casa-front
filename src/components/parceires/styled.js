a530668baa65913443beda5fcda5acae8bb8ab21
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-08-25 09:54:39 -0300

    Feat:Parceires- Solicitacao de ajustes de espacamento solicitado pela Raru @gabrielapsgomes @angeloneves19

--- src/components/parceires/styled.js
+++ src/components/parceires/styled.js
@@ -1,40 +1,36 @@
 import styled from 'styled-components'
 
 const ParceireComponents = styled.section`
-
-.parceires {
-  height: 697px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  .parceires {
+    height: 697px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
   h2 {
     color: black;
-    display:flex;
+    display: flex;
     justify-content: center;
     font-size: 2.5rem;
-    padding-top:20px;
+    padding-top: 20px;
     margin-bottom: 10px;
   }
-  
+
   p {
     color: black;
     margin-bottom: 40px;
-    display:flex;
+    display: flex;
     justify-content: center;
   }
-
-
 
   div .img-perfil {
     width: 40%;
     height: 150px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
     border-radius: 50%;
     margin-left: 30%;
   }
-
 
   h3 {
     color: black;
@@ -42,70 +38,74 @@ const ParceireComponents = styled.section`
     margin-bottom: 3px;
   }
 
-    p {
-      color: black;
-      text-align: center;
-    }
+  p {
+    color: black;
+    text-align: center;
+  }
 
-    
-    .icones-rede-sociais{
-      display:flex;
-      justify-content: center;
-      border-radius: none;
-      
-      a{
-        color: black;
-        display: flex;
-        justify-content: center;
-        height: 20px;
-        width: 20px;
-        margin-left: 10px;     
-      }
-    }   
-    
-    .perfil > .perfil-descricao {
-      padding: 20px;
-    }
-      
-
-    .styled-button{
+  .icones-rede-sociais {
     display: flex;
     justify-content: center;
-    padding:30px;
+    border-radius: none;
 
-      .button-parceire{
-        background: #516B84;
-        border-radius: 3px;
-        border: none;
-        font-size: 14px;
-        width: 182px;
-        height: 40px;
-        cursor: pointer;
+    a {
+      color: black;
+      display: flex;
+      justify-content: center;
+      height: 20px;
+      width: 20px;
+      margin-left: 10px;
+    }
+  }
+
+  .perfil-descricao {
+    margin-left: 14%;
+    width: 70%;
+  }
+
+  /* .perfil{
+    margin-right: 20px;
+  } */
+
+  .styled-button {
+    display: flex;
+    justify-content: center;
+    padding: 30px;
+
+    .button-parceire {
+      background: #516b84;
+      border-radius: 3px;
+      border: none;
+      font-size: 14px;
+      width: 182px;
+      height: 40px;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      a {
+        color: black;
+        text-decoration: none;
+        color: white;
         display: flex;
         justify-content: center;
-        align-items: center;
-
-        a{
-          color: black;
-          text-decoration: none;
-          color: white;
-          display: flex;
-        justify-content: center;
-        }
       }
     }
+  }
 
-    @media (max-width: 760px) {
-      .swiper-button-next {
-        right: 20px;
-        transform: rotate(90deg);
-      }
 
-      .swiper-button-prev {
-        left: 20px;
-        transform: rotate(90deg);
-      }
-    }  
+  @media (max-width: 760px) {
+    .swiper-button-next {
+      right: 20px;
+      transform: rotate(90deg);
+    }
 
+    .swiper-button-prev {
+      left: 20px;
+      transform: rotate(90deg);
+    }
+  }
 `
+
 export default ParceireComponents
