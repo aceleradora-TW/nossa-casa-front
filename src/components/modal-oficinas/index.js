8b980276c6fe3a6d52f8b16e54b99ae1cb9a9778
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-08-29 11:39:26 -0300

    Ajustando css da modal de oficinas - @gabrielapsgomes @angeloneves19

--- src/components/modal-oficinas/index.js
+++ src/components/modal-oficinas/index.js
@@ -158,11 +158,13 @@ const Detalhes = styled.div`
     margin-left: 43%;
     margin-top: 200px;
   }
-  @media screen and (min-width: 720px) {
+  
+@media screen and (max-width: 2560px) and (max-height: 1600px) {
     .title {
+      padding-top: -20%;
       font-size: 30px;
       padding-left: 30px;
-      padding-bottom: 40px;
+      padding-bottom: 20px;
     }
 
     .data-inicio,
@@ -186,6 +188,7 @@ const Detalhes = styled.div`
       font-weight: 900;
       padding-left: 30px;
       padding-top: 20px;
+      
     }
 
     .descricaoCMS {
@@ -198,7 +201,7 @@ const Detalhes = styled.div`
     }
 
     .inscricaoIcon {
-      margin-top: -17.8%;
+      margin-top: -16.5%;
     }
 
     .inscricao {
@@ -228,114 +231,13 @@ const Detalhes = styled.div`
     .day {
       padding-bottom: 20px;
       padding-left: 30px;
-    }
-  }
-
-  /* @media screen and (min-width: 768px) {
-    .title {
-      font-weight: 900;
-      font-size: 30px;
-      padding-left: 30px;
-      padding-bottom: 40px;
-    }
-
-    .data-inicio,
-    .data-fim {
-      font-weight: 500;
-      padding-left: 15px;
-      font-size: 17px;
-      padding-bottom: 20px;
-    }
-
-    .local,
-    .parceires {
-      font-weight: 500;
-      padding-left: 30px;
-      font-size: 17px;
-      padding-bottom: 50px;
-      padding-top: 14px;
-    }
-
-    .description {
-      font-weight: 900;
-      font-size: 30px;
-      padding-left: 30px;
-      padding-top: 20px;
-      padding-bottom: 20px;
-    }
-
-    .descricaoCMS {
-      padding-left: 30px;
-      font-size: 17px;
-      text-align: justify;
-      width: 85%;
-    }
-
-    .button-close,
-    .button-container {
-      padding-left: 90px;
-    }
-
-    .div-preco,
-    .oficinaType {
-      margin-top: -6%;
-      padding-left: 70%;
-    }
-
-    .inscricaoIcon {
-      margin-top: -17.8%;
-      padding-left: 70%;
-    }
-
-    .inscricao {
-      margin-top: -4%;
-      padding-left: 30px;
-    }
-
-    .spacingMoney,
-    .spacingType {
-      margin-left: 30px;
-      margin-top: -14px;
-    }
-
-    .spacingDate,
-    .spacingLocal {
-      margin-left: 21px;
-      margin-top: -16px;
-    }
-
-    .parce {
-      margin-left: 16px;
-      margin-top: -16px;
-    }
-
-    .spacing-parceires {
-      display: inline-block;
-      padding-left: 5px;
-    }
-
-    .day {
-      padding-bottom: 20px;
-      padding-left: 30px;
-      font-weight: 400;
-      font-size: 23px;
     }
 
     .button-inscricao {
-      background: #f5bc4a;
-      border-radius: 3px;
-      border: none;
-      font-size: 14px;
-      width: 10%;
-      height: 40px;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-left: 43%;
-      margin-top: 50px;
-    }
-  } */
+    margin-top: 50px;
+  }
+  }
+
 `
 const ModalWorkshops = ({ workshops = {} }) => {
   const [showModal, setShowModal] = useState(false)
