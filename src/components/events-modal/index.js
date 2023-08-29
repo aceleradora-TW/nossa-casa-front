5c2a3e3f2a2d8b74848eb8ac7c90294d0269e17e
Pablo Hunter <aceleradora@MacBook-Pro-do-Pablo.local>
2023-08-29 14:20:10 -0300

    styled(events-modal/index): modificando modal conforme o protopipo - @PabloHunter -@Jcdantss

--- src/components/events-modal/index.js
+++ src/components/events-modal/index.js
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
 import { useState } from 'react'
 import closeIcon from '../galery-modal/close-icon.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons'
 
 const ModalEvents = ({ events = {} }) => {
   const indexWeek = events.date.getDay()
@@ -9,50 +11,48 @@ const ModalEvents = ({ events = {} }) => {
   const handleClick = () => {
     setShowModal(!showModal)
   }
-  const handleMonth = (date) => {
-    const month = date.toLocaleDateString('pt-BR', { timeZone: 'UTC', month: 'numeric' })
-    return [month].join(' ')
+  const dateFormat = (date) => {
+    return date.slice(0, 5)
   }
 
-  const handleDay = (date) => {
-    const day = date.toLocaleDateString('pt-BR', { timeZone: 'UTC', day: 'numeric' })
-    return [day].join(' ')
+  const handleDate = (date) => {
+    const day = date.toLocaleDateString(undefined, { day: 'numeric', timeZone: 'UTC' })
+    const month = date.toLocaleDateString('pt-BR', { month: 'long' })
+    const year = date.toLocaleDateString(undefined, { year: 'numeric' })
+    return [day, month, year].join(' ')
   }
-
   return (
     <>
       <button className='button' onClick={handleClick}>Ver Mais</button>
-        <Modal
-          isOpen={showModal}
-          onRequestClose={handleClick}
-          className="modal"
-          overlayClassName="modal-overlay"
-        >
-          <header>
-            <img className='exitModalEvents' src={closeIcon} onClick={handleClick} />
-            <div className='container-description'>
-              <div className='title-events'>
-                <h1>{events.name}</h1>
-              </div>
-              <div className='dia-events'>
-                <p>{daysWeek[indexWeek]}
-                {handleDay(events.date)}
-                {handleMonth(events.date)}
-
-                </p>
-              </div>
-              <div className='dia-hora-eventos'>
-              <p>{events.time_start}</p>
-              <p>{events.time_end}</p>
-              </div>
-              <p className='local-events'>{events.location}</p>
-              <p>nome dos parceires</p>
-              <p className='preco-events'>{events.price}</p>
-              <p>{events.type}</p>
-              <p>{events.description}</p>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={handleClick}
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <header>
+          <img className='exitModalEvents' src={closeIcon} onClick={handleClick} />
+          <div className='container-description'>
+            <div className='title-events'>
+              <h1>{events.name}</h1>
             </div>
-          </header>
-        </Modal>
+            <div className='dia-events'>
+              <p>{`${daysWeek[indexWeek]} - ${dateFormat(events.date.toLocaleDateString('pt-br', { timeZone: 'UTC' }))}`}</p>
+            </div>
+            <div className='dia-hora-eventos'>
+              <div>
+              <p>
+                <FontAwesomeIcon icon={faCalendarDays} />{` ${handleDate(events.date)} ${events.time_start} ${events.time_end}`}</p>
+              </div>
+            </div>
+            <p className='local-events'>{events.location}</p>
+            <p>nome dos parceires</p>
+            <p className='preco-events'>{events.price}</p>
+            <p>{events.type}</p>
+            <p>{events.description}</p>
+          </div>
+        </header>
+      </Modal>
     </>)
 }
 
