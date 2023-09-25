5ddea842b44c0a0cc03181325ad8b5f6f3d19520
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-09-25 14:23:08 -0300

    Adicionando detalhes da oficina em sua pagina @gabrielapsgomes @angeloneves19

--- src/pages/workshops-description/index.js
+++ src/pages/workshops-description/index.js
@@ -10,26 +10,28 @@ import {
   faCalendarDays,
   faLocationDot,
 } from '@fortawesome/free-solid-svg-icons'
+import { useParams } from 'react-router'
 
-export const DetailsWorkshops = ({ workshops = {} }) => {
-  const [attributes, setAttributes] = useState([])
+export const DetailsWorkshops = () => {
+  const [workshops, setWorkshops] = useState([])
+  const { id } = useParams()
   useEffect(() => {
     cms
-      .get('api/workshops/?populate=foto_divulgacao, parceires, foto_oficina')
+      .get(`api/workshops/${id}`)
       .then((response) => {
         const { data } = response.data
-        const workshops = data.map((data) => {
-          return data.attributes
-        })
-        const parceires = workshops.map((a) =>
+        setWorkshops(data)
+      })
+      const parceires = workshops.map((a) =>
           a.parceires?.data.map((b) => b.attributes.nome)
         )
         const workshopsSortedByName = workshops.sort((a, b) =>
           a.nome < b.nome ? -1 : 1
         )
-        setAttributes(workshopsSortedByName, parceires)
-      })
+        setWorkshops(workshopsSortedByName, parceires)
   }, [])
+
+  console.log(workshops)
 
   const handleDate = (date) => {
     const day = date.toLocaleDateString(undefined, { day: 'numeric' })
@@ -48,7 +50,7 @@ export const DetailsWorkshops = ({ workshops = {} }) => {
     return [day].join(' ')
   }
 
-  const dataEmObjDate = new Date(workshops.data_inicio)
+  const dataEmObjDate = new Date(workshops?.attributes?.data_inicio)
   const indexWeek = dataEmObjDate.getDay()
   const daysWeek = [
     'Domingo',
@@ -61,26 +63,27 @@ export const DetailsWorkshops = ({ workshops = {} }) => {
   ]
 
   return (
+
     <>
+    
       <section>
         <Detalhes>
           <ul>
-            {attributes.map((workshops, key) => (
-              <li key={key}>
-                <h1 className="title">{workshops.nome}</h1>
+              <li>
+                <h1 className="title">{workshops.attributes?.nome}</h1>
                 <p className="day">
                   {daysWeek[indexWeek]} -{' '}
-                  {handleDay(new Date(workshops.data_inicio))}/
-                  {handleMonth(new Date(workshops.data_inicio))}
+                  {handleDay(new Date(workshops.attributes?.data_inicio))}/
+                  {handleMonth(new Date(workshops.attributes?.data_inicio))}
                 </p>
                 <div className="data-inicio">
                   <p className="data-inicio">
                     <FontAwesomeIcon icon={faCalendarDays} />{' '}
                     <div className="spacingDate">
-                      {handleDate(new Date(workshops.data_inicio))} •{' '}
-                      {workshops.horario_inicio} {'> '}
-                      {handleDate(new Date(workshops.data_fim))} •{' '}
-                      {workshops.horario_fim}
+                      {handleDate(new Date(workshops.attributes?.data_inicio))} •{' '}
+                      {workshops.attributes?.horario_inicio} {'> '}
+                      {handleDate(new Date(workshops.attributes?.data_fim))} •{' '}
+                      {workshops.attributes?.horario_fim}
                     </div>
                   </p>
                 </div>
@@ -94,31 +97,31 @@ export const DetailsWorkshops = ({ workshops = {} }) => {
                         '--fa-primary-opacity': '1',
                       }}
                     />
-                    <div className="spacingMoney">{workshops.preco}</div>
+                    <div className="spacingMoney">{workshops.attributes?.preco}</div>
                   </p>
                 </div>
                 <div className="div-local">
                   <p className="local">
                     <FontAwesomeIcon icon={faLocationDot} />
                     <div className="spacingLocal">
-                      Oficina presencial em {workshops.local}
+                      Oficina presencial em {workshops.attributes?.local}
                     </div>
                   </p>
                 </div>
                 <div className="oficinaType">
                   <p className="type">
                     <FontAwesomeIcon icon={faLock} />
-                    <div className="spacingType">{workshops.tipo}</div>
+                    <div className="spacingType">{workshops.attributes?.tipo}</div>
                   </p>
                 </div>
                 <div className="parceires">
                   <p className="parceire">
                     <FontAwesomeIcon icon={faUser} />
                     <div className="parce">
-                      {workshops.parceires?.data.map((parceire, index) => {
+                      {workshops.attributes?.parceires?.data.map((parceire, index) => {
                         return (
                           <p key={index} className="spacing-parceires">
-                            {parceire.attributes.nome}{' '}
+                            {parceire.attributes?.nome}{' '}
                           </p>
                         )
                       })}
@@ -126,18 +129,18 @@ export const DetailsWorkshops = ({ workshops = {} }) => {
                   </p>
                 </div>
                 <p className="description">Descrição da oficina</p>
-                <p className="descricaoCMS">{workshops.descricao}</p>
-                {workshops.url_inscricao !== null && (
+                <p className="descricaoCMS">{workshops.attributes?.descricao}</p>
+                {workshops.attributes?.url_inscricao !== null && (
                   <a
                     className="button-inscricao"
-                    href={workshops.url_inscricao}
+                    href={workshops.attributes?.url_inscricao}
                     target="_blank"
                     rel="noreferrer"
                   >
                     Inscreva-se
                   </a>
                 )}
-                {workshops.url_inscricao == null && (
+                {workshops.attributes?.url_inscricao == null && (
                   <>
                     <div className="inscricaoIcon">
                       <FontAwesomeIcon icon={faPenToSquare} />
@@ -146,10 +149,10 @@ export const DetailsWorkshops = ({ workshops = {} }) => {
                   </>
                 )}
               </li>
-            ))}
           </ul>
         </Detalhes>
       </section>
-    </>
+      
+    </> 
   )
 }
