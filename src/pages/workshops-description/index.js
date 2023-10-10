69936c6bf3a050e2ad888410a2abdf516a3def31
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-10-10 11:38:05 -0300

    Refactor(src/components/pages/workshops-description/index.js/styled.js): ajustes na respansividade -@angeloneves19 -@claudionsc

--- src/pages/workshops-description/index.js
+++ src/pages/workshops-description/index.js
@@ -2,7 +2,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Details from './styled'
 import { useState, useEffect } from 'react'
 import { cms } from '../../service/client'
-import {faUser, faPenToSquare, faLock, faHandHoldingDollar, faCalendarDays,faLocationDot,  faRectangleXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faUser,
+  faPenToSquare,
+  faLock,
+  faHandHoldingDollar,
+  faCalendarDays,
+  faLocationDot,
+  faRectangleXmark,
+} from '@fortawesome/free-solid-svg-icons'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
 import env from 'react-dotenv'
@@ -35,7 +43,7 @@ export const DetailsWorkshops = () => {
         setWorkshops(data)
       })
   }, [])
-  
+
   const handleDate = (date) => {
     const day = date.toLocaleDateString(undefined, {
       day: 'numeric',
@@ -65,16 +73,16 @@ export const DetailsWorkshops = () => {
     <>
       <section>
         <Details>
+          <Link className="closeButton" to={'/workshops'}>
+            <FontAwesomeIcon
+              icon={faRectangleXmark}
+              size="2xl"
+              style={{ color: '#ff4013' }}
+            />
+          </Link>
+          <h1 className="title">{workshops.attributes?.nome}</h1>
           <ul>
             <li>
-              <Link className="closeButton" to={'/workshops'}>
-                <FontAwesomeIcon
-                  icon={faRectangleXmark}
-                  size="2xl"
-                  style={{ color: '#ff4013' }}
-                />
-              </Link>
-              <h1 className="title">{workshops.attributes?.nome}</h1>
               <p className="day">
                 {daysWeek[indexWeek]} -{' '}
                 {new Date(workshops.attributes?.data_inicio).toLocaleDateString(
@@ -83,7 +91,7 @@ export const DetailsWorkshops = () => {
               </p>
               <div className="start-date">
                 <p className="start-date">
-                  <FontAwesomeIcon icon={faCalendarDays} size="lg"/>{' '}
+                  <FontAwesomeIcon icon={faCalendarDays} size="lg" />{' '}
                   <div className="spacingDate">
                     {handleDate(new Date(workshops.attributes?.data_inicio))} •{' '}
                     {workshops.attributes?.horario_inicio} {'> '}
@@ -93,6 +101,8 @@ export const DetailsWorkshops = () => {
                   </div>
                 </p>
               </div>
+            </li>
+            <li>
               <div className="div-price">
                 <p className="price">
                   <FontAwesomeIcon
@@ -108,14 +118,18 @@ export const DetailsWorkshops = () => {
                   </div>
                 </p>
               </div>
+            </li>
+            <li>
               <div className="div-local">
                 <p className="local">
-                  <FontAwesomeIcon icon={faLocationDot} size="lg"/>
+                  <FontAwesomeIcon icon={faLocationDot} size="lg" />
                   <div className="spacingLocal">
                     Oficina presencial em {workshops.attributes?.local}
                   </div>
                 </p>
               </div>
+            </li>
+            <li>
               <div className="workshopType">
                 <p className="type">
                   <FontAwesomeIcon icon={faLock} size="lg" />
@@ -124,47 +138,55 @@ export const DetailsWorkshops = () => {
                   </div>
                 </p>
               </div>
+            </li>
+            <li>
               <div className="partners">
                 <p className="partner">
                   <div className="parce">
                     {workshops.attributes?.parceires?.data.map(
                       (parceire, index) => {
-                        if(parceire !== null  || parceire !== undefined){
-                        return (
-                          <>
-                          <FontAwesomeIcon icon={faUser} size="lg" />
-                          <p key={index} className="spacing-partners">
-                            {parceire.attributes?.nome}{' '}
-                          </p>
-                          </>
-                        )
-                      } return null
-                    })}
+                        if (parceire !== null || parceire !== undefined) {
+                          return (
+                            <>
+                              <FontAwesomeIcon icon={faUser} size="lg" />
+                              <p key={index} className="spacing-partners">
+                                {parceire.attributes?.nome}{' '}
+                              </p>
+                            </>
+                          )
+                        }
+                        return null
+                      }
+                    )}
                   </div>
                 </p>
               </div>
-              <p className="description">Descrição da oficina</p>
-              <p className="descriptionCMS">{workshops.attributes?.descricao}</p>
-              {workshops.attributes?.url_inscricao !== null && (
-                <a
-                  className="button-inscription"
-                  href={workshops.attributes?.url_inscricao}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Inscreva-se
-                </a>
-              )}
+            </li>
+            <li>
               {workshops.attributes?.url_inscricao == null && (
                 <>
                   <div className="inscriptionIcon">
-                    <FontAwesomeIcon icon={faPenToSquare} size="lg"/>
+                    <div>
+                      <FontAwesomeIcon icon={faPenToSquare} size="lg" />
+                    </div>
                     <h1 className="inscription"> Inscrição não é necessária</h1>
                   </div>
                 </>
               )}
             </li>
           </ul>
+          <p className="description">Descrição da oficina</p>
+          <p className="descriptionCMS">{workshops.attributes?.descricao}</p>
+          {workshops.attributes?.url_inscricao !== null && (
+            <a
+              className="button-inscription"
+              href={workshops.attributes?.url_inscricao}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Inscreva-se
+            </a>
+          )}
           <section className="page">
             <div className="style-img-swiper">
               <Swiper
