d5bcb936033247980609acc897f5c29eec27a4e4
yrachid <anuar.yasser@gmail.com>
2023-10-25 13:59:24 +0100

    Remove fragmento desnecessario

--- src/pages/workshop-details/index.js
+++ src/pages/workshop-details/index.js
@@ -83,141 +83,154 @@ export const WorkshopDetails = () => {
         )}`
 
   return (
-    <>
-      <Details>
-        <section className="description-section">
-          <Link className="close-button" to={'/workshops'}>
-            <FontAwesomeIcon
-              icon={faRectangleXmark}
-              size="2xl"
-              style={{ color: 'black' }}
-            />
-          </Link>
-          <h1 className="title">{workshops.attributes?.nome}</h1>
-          <span className="span-detais">
-            <ul id="container-details">
-              <li>
-                <div className="start-date">
-                  <div className="spacingDate">
-                    <FontAwesomeIcon icon={faCalendarDays} size="lg" />
-                    <p>{formatWorkshopDates(workshops)}</p>
-                    <p>{formatWorkshopDuration(workshops)}</p>
+    <Details>
+      <section className="description-section">
+        <Link className="close-button" to={'/workshops'}>
+          <FontAwesomeIcon
+            icon={faRectangleXmark}
+            size="2xl"
+            style={{ color: 'black' }}
+          />
+        </Link>
+        <h1 className="title">{workshops.attributes?.nome}</h1>
+        <span className="span-detais">
+          <ul id="container-details">
+            <li>
+              <div className="start-date">
+                <div className="spacingDate">
+                  <FontAwesomeIcon icon={faCalendarDays} size="lg" />
+                  <p>{formatWorkshopDates(workshops)}</p>
+                  <p>{formatWorkshopDuration(workshops)}</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="div-price">
+                <p className="price">
+                  <FontAwesomeIcon
+                    icon={faHandHoldingDollar}
+                    size="lg"
+                    style={{
+                      '--fa-secondary-color': '#ffffff',
+                      '--fa-primary-opacity': '1',
+                    }}
+                  />
+                  <div className="spacingMoney">
+                    {workshops.attributes?.preco}
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="div-price">
-                  <p className="price">
-                    <FontAwesomeIcon
-                      icon={faHandHoldingDollar}
-                      size="lg"
-                      style={{
-                        '--fa-secondary-color': '#ffffff',
-                        '--fa-primary-opacity': '1',
-                      }}
-                    />
-                    <div className="spacingMoney">
-                      {workshops.attributes?.preco}
-                    </div>
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div className="div-local">
-                  <p className="local">
-                    <div className="spacingLocal">
-                      <FontAwesomeIcon icon={faLocationDot} size="lg" />
-                      Oficina presencial em {workshops.attributes?.local}
-                    </div>
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div className="workshopType">
-                  <p className="type">
-                    <FontAwesomeIcon
-                      icon={faBullhorn}
-                      style={{
-                        '--fa-primary-color': '#000000',
-                        '--fa-secondary-color': '#000000',
-                        '--fa-secondary-opacity': '1',
-                        transform: 'rotate(-15deg)',
-                      }}
-                      size="lg"
-                    />
-                    <div className="spacingType">
-                      {workshops.attributes?.tipo}
-                    </div>
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div className="partners">
-                  <p className="partner">
-                    <div className="parce">
-                      {workshops.attributes?.parceires?.data.map(
-                        (parceire, index) => {
-                          if (parceire !== null || parceire !== undefined) {
-                            return (
-                              <>
-                                <FontAwesomeIcon icon={faUser} size="lg" />
-                                <p key={index} className="spacing-partners">
-                                  {parceire.attributes?.nome}{' '}
-                                </p>
-                              </>
-                            )
-                          }
-                          return null
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="div-local">
+                <p className="local">
+                  <div className="spacingLocal">
+                    <FontAwesomeIcon icon={faLocationDot} size="lg" />
+                    Oficina presencial em {workshops.attributes?.local}
+                  </div>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="workshopType">
+                <p className="type">
+                  <FontAwesomeIcon
+                    icon={faBullhorn}
+                    style={{
+                      '--fa-primary-color': '#000000',
+                      '--fa-secondary-color': '#000000',
+                      '--fa-secondary-opacity': '1',
+                      transform: 'rotate(-15deg)',
+                    }}
+                    size="lg"
+                  />
+                  <div className="spacingType">
+                    {workshops.attributes?.tipo}
+                  </div>
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="partners">
+                <p className="partner">
+                  <div className="parce">
+                    {workshops.attributes?.parceires?.data.map(
+                      (parceire, index) => {
+                        if (parceire !== null || parceire !== undefined) {
+                          return (
+                            <>
+                              <FontAwesomeIcon icon={faUser} size="lg" />
+                              <p key={index} className="spacing-partners">
+                                {parceire.attributes?.nome}{' '}
+                              </p>
+                            </>
+                          )
                         }
-                      )}
+                        return null
+                      }
+                    )}
+                  </div>
+                </p>
+              </div>
+            </li>
+            <li>
+              {workshops.attributes?.url_inscricao == null && (
+                <>
+                  <div className="inscriptionIcon">
+                    <div>
+                      <FontAwesomeIcon icon={faPenToSquare} size="lg" />
                     </div>
-                  </p>
-                </div>
-              </li>
-              <li>
-                {workshops.attributes?.url_inscricao == null && (
-                  <>
-                    <div className="inscriptionIcon">
-                      <div>
-                        <FontAwesomeIcon icon={faPenToSquare} size="lg" />
-                      </div>
-                      <h1 className="inscription">
-                        {' '}
-                        Inscrição não é necessária
-                      </h1>
-                    </div>
-                  </>
-                )}
-              </li>
-            </ul>
-          </span>
+                    <h1 className="inscription"> Inscrição não é necessária</h1>
+                  </div>
+                </>
+              )}
+            </li>
+          </ul>
+        </span>
 
-          <p className="description">Descrição da oficina</p>
-          <p className="descriptionCMS">{workshops.attributes?.descricao}</p>
-          {workshops.attributes?.url_inscricao !== null && (
-            <a
-              className="button-inscription"
-              href={workshops.attributes?.url_inscricao}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Inscreva-se
-            </a>
-          )}
-        </section>
-        <section className="page">
-          <div className="style-img-swiper">
+        <p className="description">Descrição da oficina</p>
+        <p className="descriptionCMS">{workshops.attributes?.descricao}</p>
+        {workshops.attributes?.url_inscricao !== null && (
+          <a
+            className="button-inscription"
+            href={workshops.attributes?.url_inscricao}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Inscreva-se
+          </a>
+        )}
+      </section>
+      <section className="page">
+        <div className="style-img-swiper">
+          <Swiper
+            style={{
+              '--swiper-navigation-color': '#516B84',
+              '--swiper-pagination-color': '',
+            }}
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper2"
+          >
+            {galleryPhoto.map((image) => (
+              <SwiperSlide key={image.id}>
+                <img src={image.url} alt={image.name} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="style-swiper-fotos">
             <Swiper
-              style={{
-                '--swiper-navigation-color': '#516B84',
-                '--swiper-pagination-color': '',
-              }}
+              onSwiper={setThumbsSwiper}
               loop={true}
               spaceBetween={10}
-              navigation={true}
-              thumbs={{ swiper: thumbsSwiper }}
+              slidesPerView={7}
+              freeMode={true}
+              watchSlidesProgress={true}
               modules={[FreeMode, Navigation, Thumbs]}
-              className="mySwiper2"
+              className="second-carousel"
             >
               {galleryPhoto.map((image) => (
                 <SwiperSlide key={image.id}>
@@ -225,27 +238,9 @@ export const WorkshopDetails = () => {
                 </SwiperSlide>
               ))}
             </Swiper>
-            <div className="style-swiper-fotos">
-              <Swiper
-                onSwiper={setThumbsSwiper}
-                loop={true}
-                spaceBetween={10}
-                slidesPerView={7}
-                freeMode={true}
-                watchSlidesProgress={true}
-                modules={[FreeMode, Navigation, Thumbs]}
-                className="second-carousel"
-              >
-                {galleryPhoto.map((image) => (
-                  <SwiperSlide key={image.id}>
-                    <img src={image.url} alt={image.name} />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
           </div>
-        </section>
-      </Details>
-    </>
+        </div>
+      </section>
+    </Details>
   )
 }
