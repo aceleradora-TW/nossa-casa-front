f4ef345a91c6ef073b640e581b76491e7f2add99
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-10-31 14:00:30 -0300

    Alteracoes pedidas no pr

--- src/pages/about-details/index.js
+++ src/pages/about-details/index.js
@@ -6,15 +6,17 @@ const PageAbout  = () => {
     <>
     <NavBar/>
     <StyledAbout>
-    <h2 className="about-title">Sobre Nossa Casa</h2>
+    <h2 className="about-title">
+      Sobre Nossa Casa
+      </h2>
        <div className="content-container">
                 <div className="topic">
-                  <h3>Nossa Historia Ate Aqui</h3>
+                  <h3>Nossa história até aqui</h3>
                   <p>
-                    Em 2018 a Nossa Casa nasce com o intuito de oferecer à
-                    população acesso à serviços, produtos e atividades nas áreas
+                    Em 2018, a Nossa Casa nasce com o intuito de oferecer à
+                    população acesso a serviços, produtos e atividades nas áreas, produtos e atividades nas áreas
                     de saúde mental, terapias integrativas, cultura e arte,
-                    entendendo tais ações como promoção e garantia de direitos à
+                    entendendo tais ações como promoção e garantia de direitos a
                     todes, com foco na acessibilidade à pessoas em situação de
                     vulnerabilidade.
                   </p>
@@ -22,22 +24,22 @@ const PageAbout  = () => {
                 <div className="topic">
                   <h3>Como somos conhecidos</h3>
                   <p>
-                    A Nossa Casa passa então à ser reconhecida como um centro
-                    cultural, de formação, fomentação e garantia de direitos à
-                    todes, sobretudo à população negra, mulheres e LGBTQIAP+
+                    A Nossa Casa passa então a ser reconhecida como um centro
+                    cultural, de formação, fomentação e garantia de direitos a
+                    todes, sobretudo à população negra, mulheres e LGBTQIAP+,
                     articulando e promovendo eventos, lançamentos musicais,
                     saraus, exposições áudio visuais, palestras, oficinas, etc.
                   </p>
                 </div>
                 <div className="topic">
-                  <h3>Nosso inicio</h3>
+                  <h3>Nosso início</h3>
                   <p>
-                    No primeiro ano de funcionamento a Nossa Casa atendeu
+                    No primeiro ano de funcionamento, a Nossa Casa atendeu
                     centenas de pessoas nos serviços de psicoterapia,
                     massoterapia, yoga, nutrição, psicopedagogia, arte terapia,
-                    rodas de conversa, formações, entre outros, á valores
+                    rodas de conversa, formações, entre outros, a valores
                     acessíveis ou ainda em parceria com entidades do terceiro
-                    setor do Município atendendo seus usuários.
+                    setor do município atendendo seus usuários.
                   </p>
                 </div>
                 <div className="topic">
@@ -51,19 +53,19 @@ const PageAbout  = () => {
                 <div className="topic">
                   <h3>Profissionais</h3>
                   <p>
-                    Os profissionais que compunham a Nossa Casa, atuavam também
-                    de maneira autônoma gerando renda para si e contribuindo
+                    Os profissionais que compõem a Nossa Casa atuam também
+                    de maneira autônoma, gerando renda para si e contribuindo
                     para a manutenção do coletivo.
                   </p>
                 </div>
                 <div className="topic">
                   <h3>Como funciona</h3>
                   <p>
-                    Funcionando de maneira independente e colaborativa o
-                    coletivo embora tenha estabelecido parcerias com algumas
+                    Funcionando de maneira independente e colaborativa, o
+                    coletivo, embora tenha estabelecido parcerias com algumas
                     entidades e outros atores de coletivos semelhantes na
                     cidade, não estabeleceu nenhuma forma de conveniamento ou
-                    parceria contínua com qualquer órgão publico nesse período.
+                    parceria contínua com qualquer órgão público nesse período.
                   </p>
                 </div>
               </div>
--- src/pages/about-details/styled.js
+++ src/pages/about-details/styled.js
@@ -1,28 +1,27 @@
 import styled from 'styled-components'
 
 export const StyledAbout = styled.div`
-  max-width:auto;
-  max-height:auto;
+  max-width: auto;
+  max-height: auto;
   z-index: 10;
-  background-color: ${props => props.theme === 'about' ? '#FFFFFF' : '#fff'};
-
+  background-color: #ffffff;
 
   .about-title {
     margin-top: 5%;
-    color:#54636E;
+    color: #54636e;
     font-size: 2.25rem;
     text-align: left;
     margin-bottom: 90px;
-    margin-left:0px;
+    margin-left: 0px;
   }
   body {
     overflow-x: hidden;
-}
+  }
 
   .content-container {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr ;
+    grid-template-rows: 1fr 1fr;
     height: 70%;
     width: 90%;
     margin: auto;
@@ -32,31 +31,30 @@ export const StyledAbout = styled.div`
   .topic {
     height: 100%;
     width: 100%;
-    margin: 0 0px;;
+    margin: 0 0px;
 
     h3 {
-      color:#54636E;
+      color: #54636e;
       font-size: 1rem;
       margin-bottom: 10px;
     }
 
     p {
-      color:#000;
-      font-size:1rem;
-      text-align:justify;
+      color: #000;
+      font-size: 1rem;
+      text-align: justify;
     }
-  } 
+  }
 
-//IOS
+  //IOS
   @media (max-width: 720px) {
     .content-container {
-      height: 90%; 
+      height: 90%;
       grid-template-columns: 1fr;
       grid-template-rows: auto;
       grid-row-gap: 25px;
       grid-column-gap: 20px;
     }
-    
 
     .about-title {
       align-items: center;
@@ -67,62 +65,55 @@ export const StyledAbout = styled.div`
     }
 
     .topic {
-    width: 48%;
-    margin: 0 -30px;
+      width: 48%;
+      margin: 0 -30px;
 
       h3 {
-        width: 70%; 
+        width: 70%;
         font-size: 1.3rem;
         margin-bottom: 8px;
       }
 
       p {
-        
         font-size: 1.5rem;
       }
     }
   }
 
-@media screen and (min-width: 780px) and (max-width: 2920px)  {
+  @media screen and (min-width: 780px) and (max-width: 2920px) {
+    content-container {
+      text-align: justify;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+      grid-row-gap: 55px;
+      grid-column-gap: 20px;
+      margin: 0 150px;
+      padding: 10px;
+      overflow: hidden;
+    }
 
-.content-container   {
-text-align:justify;
-grid-template-columns: 1fr 1fr ;
-grid-template-rows: auto;
-grid-row-gap: 55px;
-grid-column-gap: 20px;
-margin: 0 150px;
-padding: 10px;
-overflow: hidden;
-}
+    .about-title {
+      display: block;
+      text-align: left;
+      width: 38%;
+      margin-left: 11.3rem;
+      font-size: 1.9rem;
+      margin-bottom: 3%;
+    }
 
-.about-title   {
+    .topic {
+      width: 70%;
+      margin: 0 20px;
 
-display: block;
-text-align: left;
-width: 38%;
-margin-left: 11.3rem;
-font-size: 1.9rem;
-margin-bottom:3%; 
+      h3 {
+        font-size: 1.3rem;
+        margin-bottom: 15px;
+      }
 
-}
-
-
-.topic {
-width: 70%; 
-margin: 0 20px;
-
-h3 { 
-
-  font-size: 1.3rem;
-  margin-bottom: 15px;
-}
-
-p {
-  font-size: 1rem;
-  line-height:1.5rem;
-}
-}   
+      p {
+        font-size: 1rem;
+        line-height: 1.5rem;
+      }
+    }
   }
-
 `
