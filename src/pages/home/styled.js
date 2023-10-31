22dbf8b83e7806ce52e2711737cfd1035387ae0f
joyce-caroline <caroline.joyce5631@gmail.com>
2023-10-31 10:18:06 -0300

    refactor(src/pages/home): Ajusta imagens da galeria - @wander06 @joyce-caroline

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -124,7 +124,7 @@ const Home = styled.div`
     }
 
     .galeria {
-      padding-top: 90px;
+      padding-top: 30px;
       background-color: #fafafa;
       display: flex;
       flex-direction: column;
@@ -137,7 +137,6 @@ const Home = styled.div`
         margin-left: 8.3%;
         font-weight: 700;
         line-height: 64px;
-        text-align: center;
         font-size: 3.125rem;
         color: #516b84;
         cursor: default;
@@ -148,26 +147,24 @@ const Home = styled.div`
         align-self: normal;
         margin: 0 8.3%;
 
+
         .painel {
           width: 100%;
           display: grid;
           grid-template-columns: repeat(3, 1fr);
-          grid-template-rows: minmax(2, 1fr);
-          grid-column-gap: 20px;
-          grid-row-gap: 20px;
-
-          li{
-            border: 1px solid;
-          }
+          grid-template-rows: repeat(2, 1fr);
+          grid-column-gap: 1%;
+          grid-row-gap: 2%;
+          justify-content: center;
 
           li img {
             width: 100%;
             height: 100%;
-            /* max-height: 312px;
-            max-width: 800px; */
-            /* margin-left: 2%; */
-            border-radius: 3px;
-            margin-bottom: 0.5rem;
+            max-height: 400px;
+            max-width: 1000px;
+            border-radius: 4px;
+            object-fit: cover;
+            object-position: center;
           }
         }
       }
