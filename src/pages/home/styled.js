061fe5a6586dec4ec78fc665c7cbf8664f60dfac
joyce-caroline <caroline.joyce5631@gmail.com>
2023-11-07 13:51:44 -0300

    style(pages/home): Finaliza responsividade na seção About - @Ester-Arruda @joyce-caroline

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -101,279 +101,154 @@ const Home = styled.div`
         display: flex;
         justify-content: space-around;
       }
-        @media (max-width: 767px) {
-          flex-wrap: wrap;
-          height: 100%;
-        }
-
-        .text {
-          box-sizing: border-box;
-          text-align: justify;
-          padding: 2% 2% ;
-          height: 100%;
-          width: 100%;
-          max-width: 800px;
-          h2 {
-            font-size: 26px;
-            margin-bottom: 30px;
-          }
-          align-items: center;
-        }
-        .foto {
-          height: 100%;
-          width: 100%;
-          max-width: 800px;
-          margin-left: 3%;
-          border-radius: 3px;
-          margin-bottom: 4.6rem;
-          img {
-            height: 100%;
-            width: 100%;
-            max-width: 100%;
-          }
-        }
-      }
-
-      button {
-        background: #f5bc4a;
-        box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.4);
-        border-radius: 3px;
-        color: #000000;
-        border: none;
-        font-size: 1rem;
-        width: 163px;
-        height: 56px;
-        font-weight: 600;
-        cursor: pointer;
-        padding: 10px;
-        margin-top: 1.6rem;
-        margin-bottom: 1.6rem;
-      }
-
-      @media (max-width: 500px) {
-       .about .content {
-          flex-direction: column;
-          height: initial;
-          justify-content: initial;
-          padding: 2vh;
-
-        }
-
-        .about {
-          justify-content: initial;
-          height: initial;
-        }
-
-        .about .foto {
-          order: -1;
-          margin-bottom: 2rem;
-          margin-left: 0;
-        }
-
-        .about .text {
-          display: flex;
-          flex-direction: column;
-          padding: 0;
-        }
-      }
-
-      @media (max-width: 320px) {
+      @media (max-width: 767px) {
         flex-wrap: wrap;
         height: 100%;
       }
-    }
 
-    .galeria {
-      max-width: 1200px;
-      margin: 0 auto;
-      background-color: #fafafa;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 30px;
-
-      .titulo-galeria {
-        align-self: flex-start;
-        margin-left: 12.5%;
-        font-size: 2.25rem;
-        font-weight: 700;
-        color: #516b84;
-        line-height: 64px;
-        text-align: center;
-        font-size: 3.125rem;
-        cursor: default;
-      }
-
-      .painel {
-        width: 75%;
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-column-gap: 25px;
-        grid-row-gap: 25px;
-        overflow: hidden;
-
-        img {
-          width: 100%;
-          height: 100%;
-          max-width: auto;
-          margin-left: 3%;
-          border-radius: 3px;
-          margin-bottom: 3.5rem;
+      .text {
+        box-sizing: border-box;
+        text-align: justify;
+        padding: 2% 2%;
+        height: 100%;
+        width: 100%;
+        max-width: 800px;
+        h2 {
+          font-size: 26px;
+          margin-bottom: 30px;
         }
+        align-items: center;
       }
-      .container-painel {
-        display: flex;
-        justify-content: center;
-      }
-      button {
-        background: #516b84;
+      .foto {
+        height: 100%;
+        width: 100%;
+        max-width: 800px;
+        margin-left: 3%;
         border-radius: 3px;
-        color: #fafafa;
-        border: none;
-        font-size: 0.75rem;
-        width: 182px;
-        height: 40px;
-        cursor: pointer;
-      }
-    }
-
-
-  @media (max-width: 1280px) {
-    .galeria{
-      height: 950px;
-    }
-    }
-
-    .parceires {
-      background-color: aliceblue;
-      height: 697px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      h2 {
-        font-size: 2.5rem;
-        margin-top: 30px;
-        margin-bottom: 10px;
-        cursor: default;
-      }
-
-      p {
-        margin-bottom: 40px;
-        cursor: default;
-      }
-
-      .carrossel-perfis {
-        height: 60%;
-        display: flex;
-        justify-content: center;
-        gap: 20px;
-        cursor: default;
-
-        .perfil {
-          background-color: cadetblue;
-          width: 30%;
-
-          h3 {
-            text-align: center;
-            cursor: default;
-          }
-
-          p {
-            text-align: center;
-            cursor: default;
-          }
+        margin-bottom: 4.6rem;
+        img {
+          height: 100%;
+          width: 100%;
+          max-width: 100%;
         }
       }
     }
-    
-    .about {
-    background-color: #ffffff;
+
+    button {
+      background: #f5bc4a;
+      box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.4);
+      border-radius: 3px;
+      color: #000000;
+      border: none;
+      font-size: 1rem;
+      width: 163px;
+      height: 56px;
+      font-weight: 600;
+      cursor: pointer;
+      padding: 10px;
+      margin-top: 1.6rem;
+      margin-bottom: 1.6rem;
+    }
+
+    @media (max-width: 900px) {
+      section.about{
+        margin-bottom: 0;
+      }
+      .about .content {
+        height: auto;
+      }
+
+      .text button{
+        margin-bottom: 0;
+      }
+    }
+
+    @media (max-width: 500px) {
+      .about .content {
+        flex-direction: column;
+        height: initial;
+        justify-content: initial;
+        padding: 2vh;
+      }
+
+      .about {
+        justify-content: initial;
+        height: initial;
+      }
+
+      .about .foto {
+        order: -1;
+        margin-bottom: 2rem;
+        margin-left: 0;
+      }
+
+      .about .text {
+        display: flex;
+        flex-direction: column;
+        padding: 0;
+      }
+    }
+
+    @media (max-width: 320px) {
+      flex-wrap: wrap;
+      height: 100%;
+    }
+  }
+
+  .galeria {
+    max-width: 1200px;
+    margin: 0 auto;
+    background-color: #fafafa;
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
-    box-sizing: border-box;
-    align-items: center;
-    height: 627px;
-  }
-
-main .about {
-  height: auto;
-  padding: 10px;
-}
-
-  main .about .content {
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    width: 100%;
-    flex-direction: row;
-    margin-top: 5rem;
-    margin-bottom: 4rem;
-  }
-
-  .content .text {
-    box-sizing: border-box;
-    text-align: justify;
-    width: 40%;
-    margin-bottom: auto;
-  }
-
-  .foto img {
-    width: 110%;
-    max-width: auto;
-    margin-left: 3%;
-    border-radius: 3px;
-    margin-bottom: 4.6rem;
-  }
-
-  @media screen and (min-width: 720px) and (max-width:2920px) {
-    .content > div {
-      width: auto;
-      max-width: auto;
-    }
-
-    .content h2 {
-      font-size: 2.25rem;
-      margin-bottom: 30px;
-    }
-
-    .content .Textparagraph {
-      font-size: 1.4rem;
-      font-weight: 500;
-      line-height: 35px;
-    }
-
-    .content .text h2 {
-      font-size: 2rem;
-      color: #516B84;
-      cursor: default;
-    }
-
-    .content .text .Textparagraph {
-      font-size: 25px;
-      color: #54636F;
-      cursor: default;
-    }
-  }
-  .button-about{
-    color: black;
-    font-weight: bold;
-    text-decoration: none;
-  }
-
-  .styled-button {
-    display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #f5bc4a;
-    width: 146px;
-    height: 40px;
-    border: none;
-    margin-top: 1rem;
-  }
+    gap: 30px;
 
+    .titulo-galeria {
+      align-self: flex-start;
+      margin-left: 12.5%;
+      font-size: 2.25rem;
+      font-weight: 700;
+      color: #516b84;
+      line-height: 64px;
+      text-align: center;
+      font-size: 3.125rem;
+      cursor: default;
+    }
+
+    .painel {
+      width: 75%;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-column-gap: 25px;
+      grid-row-gap: 25px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+        max-width: auto;
+        margin-left: 3%;
+        border-radius: 3px;
+        margin-bottom: 3.5rem;
+      }
+    }
+    .container-painel {
+      display: flex;
+      justify-content: center;
+    }
+    button {
+      background: #516b84;
+      border-radius: 3px;
+      color: #fafafa;
+      border: none;
+      font-size: 0.75rem;
+      width: 182px;
+      height: 40px;
+      cursor: pointer;
+    }
+  }
 `
 
 export default Home
