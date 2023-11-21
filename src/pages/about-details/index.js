a0c5466ac1194c991dc2bfe51277e2e15415e4c6
Wander06 <wandersonpsilva7@gmail.com>
2023-11-21 13:11:46 -0300

    Feat(nossa-casa-front/src/pages/about-details/index.js): Continuando o mapeamento da pagina about-details - @wander06 @jcdantss

--- src/pages/about-details/index.js
+++ src/pages/about-details/index.js
@@ -8,21 +8,27 @@ const PageAbout = () => {
   useEffect(() => {
     cms.get('api/plural-sobre-nos/?populate=foto').then((response) => {
       const { data } = response.data
-      const aboutDescription = data.map(description => description.attributes)
+      console.log(data)
+      const aboutDescription = data.map(description => {
+        if(description.attributes){
+          return {
+            descricao1: description.attributes.descricao1,
+            descricao2: description.attributes.descricao2,
+            descricao3: description.attributes.descricao3,
+            descricao4: description.attributes.descricao4,
+            descricao5: description.attributes.descricao5
+          }
+        }})
       setAbout(aboutDescription)
-      console.log(aboutDescription)
     })
-
   }, [])
+  console.log(about[0].descricao1)
   return (
     <>
       <StyledAbout>
         <div className="container">
           <div>
             <ul>
-              {about.map((index, key) =>
-                <li key={key}>{index}</li>
-              )}
             </ul>
           </div>
           <div>
