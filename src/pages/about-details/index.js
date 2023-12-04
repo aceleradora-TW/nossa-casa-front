7b3bd6194c9075769b0cecc31f491b4c7ce985e9
joyce-caroline <caroline.joyce5631@gmail.com>
2023-12-04 15:24:14 -0300

    Fix(about-details): Ajusta chamada da imagem padrão - @joyce-caroline @jCdantss

--- src/pages/about-details/index.js
+++ src/pages/about-details/index.js
@@ -1,7 +1,7 @@
-import { StyledAbout } from "./styled"
+import { StyledAbout } from './styled'
 import NavBar from '../../components/navbar'
 import { cms } from '../../client'
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 const PageAbout = () => {
   const [about, setAbout] = useState([])
@@ -10,30 +10,40 @@ const PageAbout = () => {
     cms.get('api/plural-sobre-nos/?populate=foto').then((response) => {
       const { data } = response.data
       if (data) {
-        const aboutDescription = data.map(description => {
+        const aboutDescription = data.map((description) => {
           return {
-            descricao: description.attributes.descricao,
-            foto: description.attributes.foto.data.attributes.url
+            descricao: description.attributes?.descricao,
+            foto: description.attributes?.foto?.data?.attributes?.url
           }
         })
         setAbout(aboutDescription)
       }
     })
   }, [])
+
+  //Experimentando usar o useEffect para ver o comportamento da variavel about
+  // useEffect(() => {
+  //   console.log(about)
+  // }, [about])
+
   return (
     <>
       <StyledAbout>
         <div className="container">
           <div>
             <ul>
-              {about.map((description, key) =>
+              {about.map((description, key) => (
                 <li key={key}>{description.descricao}</li>
-              )}
+              ))}
             </ul>
           </div>
-          <div>
-            <img src={urlCms + about[0].foto} />
-          </div>
+          {about.length > 0 ? (
+            <div>
+              <img src={urlCms + about[0].foto} />
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </StyledAbout>
     </>
