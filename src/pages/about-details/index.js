edfebcd422f0676f8eb89e7b778956760f042bae
joyce-caroline <caroline.joyce5631@gmail.com>
2023-12-06 13:27:07 -0300

    Feat(about-details): Tentativa de utilização do react-markdown - @jcdantss @joyce-caroline

--- src/pages/about-details/index.js
+++ src/pages/about-details/index.js
@@ -2,6 +2,7 @@ import { StyledAbout } from './styled'
 import NavBar from '../../components/navbar'
 import { cms } from '../../client'
 import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const PageAbout = () => {
   const [about, setAbout] = useState([])
@@ -25,11 +26,24 @@ const PageAbout = () => {
       <NavBar></NavBar>
       <StyledAbout>
         {about.length > 0 ? <img src={urlCms + about[0].foto} /> : <div></div>}
+
+        {/* Verifica o tipo retornado na description */}
+        {about.length > 0 ? console.log(typeof about[0].descricao ) : <div></div>}
         <ul>
-            {about.map((description, key) => (
-              <li key={key}>{description.descricao}</li>
-            ))}
-          </ul>
+          {about.map((description, key) => (
+            <li key={key}>{console.log(typeof description.descricao)}</li>
+          ))}
+        </ul>
+
+        {/* <ReactMarkdown>
+          {about.length > 0 ? (
+            <ul>
+              <li>{about[0].descricao}</li>
+            </ul>
+          ) : (
+            <div></div>
+          )}
+        </ReactMarkdown> */}
       </StyledAbout>
     </>
   )
