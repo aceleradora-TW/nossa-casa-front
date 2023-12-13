6ac80ed6cf9caafb13cd550e2afdfb2b3cdb2992
thayanneregoo <thayanneregoo@gmail.com>
2023-12-13 14:19:35 -0300

    feat(scr/components/footer/index.js): Removendo ícone do faceboook e adicionando o do linktree @angeloneves19 @thayanneregoo

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import FooterComponent from './styled'
 import Instagram from './instagram.png'
 import Whatsapp from './whatsapp.png'
-import Facebook from './facebook.png'
+import Linktree from './linktree.png'
 import NossaCasa from './nossa_casa_logo_nova.png'
 import Spotify from './spotify.png'
 
@@ -12,8 +12,8 @@ const Footer = () => {
       <img className='logo' src={NossaCasa}/>
       <div>
       <a href={'https://chat.whatsapp.com/BndMXO2VZY7CS04IiJ9An2'}><img src={Whatsapp}/></a>
-      <a href={'https://open.spotify.com/playlist/0ww2odZFyDmLVaz7Cfho4x?si=0578cc6f81c142c6'}><img src={Spotify}/></a>
-      <img src={Facebook}/>
+      <a href={'https://open.spotify.com/playlist/0wwodZFyDmLVaz7Cfho4x?si=0578cc6f81c142c6'}><img src={Spotify}/></a>
+      <a href={'https://linktr.ee/nossacasaartes'}><img src={Linktree}/></a>
       <a href={'https://www.instagram.com/nossacasa_at/'}><img src={Instagram}/></a>
       </div>
       <p>©2023 - Nossa Casa | Todos os direitos reservados</p>
