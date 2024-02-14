ef9c1f6f9bf3d429ded3cd4a6893c4bfa23663bf
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2024-02-14 14:34:52 -0300

    feat(helpers/format-data/indez.js): modificamos o utc por nao estar funcionando o metodo sugerido no PR -@angeloneves19 -@thaynneregoo -@ester-arruda

--- src/helpers/format-data/index.js
+++ src/helpers/format-data/index.js
@@ -1,12 +1,12 @@
 const formatDate = (date) => {
   const dateFormated = new Intl.DateTimeFormat('pt-BR', {
-    timeZone: 'UTC',
+    timeZone: 'America/Sao_Paulo',
     day: '2-digit',
     month: 'short',
     year: 'numeric',
     weekday: 'long',
   }).format(date)
-  return dateFormated[0].toUpperCase()+ dateFormated.slice(1,)
+  return dateFormated[0].toUpperCase() + dateFormated.slice(1)
 }
 
 export const formatWorkshopDuration = (workshop) =>
